4a8584d0ce1a8fd645f0b48786cc131d1982d8de
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-21 12:17:53 +0000

    add rekaltion with user

---
+++ app/Models/Tournois.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Tournois extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date'
+       ];
+    public function user(){
+        return $this->BelongsTo(User::class);
+    }
+}
