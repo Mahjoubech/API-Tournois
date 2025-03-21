387a88006d112a489c8217218c151ff6ae1226e4
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-21 12:09:16 +0000

    change route for auth and add apiResource for tournois

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TournoisController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -20,5 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::get('/user', [AuthController::class, 'user'])->middleware('auth:api');
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::apiResource('tournois',TournoisController::class);
