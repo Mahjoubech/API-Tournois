7cdc75cb0f61ac9a3e6966eabf425249d434a864
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-21 12:18:15 +0000

    add crud tournois

---
+++ app/Http/Controllers/TournoisController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tournois;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use OpenApi\Annotations as OA;
+
+class TournoisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(    Tournois::all());
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+{
+    $trn = $request->validate([
+        'name' => 'required|string|max:255',
+        'description' => 'nullable|string',
+        'start_date' => 'required|date',
+        'end_date' => 'nullable|date|after_or_equal:start_date',
+    ]);
+
+    if (!$request->user()) {
+        return response()->json(['message' => 'Utilisateur non authentifié'], 401);
+    }
+
+    $trnois = $request->user()->tournois()->create($trn);
+
+    return response()->json($trnois, 201);
+}
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tournois $tournois)
+    {
+
+        if (!$tournois) {
+            return response()->json(['message' => 'Tournoi non trouvé'], 404);
+        }
+
+        return response()->json($tournois);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tournois $tournois)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tournois $tournois)
+    {
+
+            Gate::authorize('modify', $tournois);
+            if (!$tournois) {
+                return response()->json(['message' => 'Tournoi non trouvé'], 404);
+            }
+            $data = $request->validate([
+             'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_date' => 'sometimes|date',
+            'end_date' => 'nullable|date|after_or_equal:start_date',
+            ]);
+            $tournois->update($data);
+            return response()->json($tournois);
+        }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tournois $tournois)
+    {
+        Gate::authorize('modify', $tournois);
+        $tournois->delete();
+        return ['message' => 'The Country was deleted'];
+    }
+}
